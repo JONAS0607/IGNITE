699e2d7b1d9446c1e3d39e3fadd6158e79e7d003
Jonas Cabral <cabral.j@unoesc.edu.br>
2022-02-19 16:17:02 -0300

    Refatorando a listagem de categoria

--- PROJETO_02_RENTALX/src/routes/categories.routes.ts
+++ PROJETO_02_RENTALX/src/routes/categories.routes.ts
@@ -1,16 +1,14 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
-const categories_routes = Router();
-const categories_repository = new CategoriesRepository();
+const categoriesRoutes = Router();
 
-categories_routes.post("/", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
-categories_routes.get("/", (request, response) => {
-  const all = categories_repository.list();
-  return response.status(200).json({ all });
+categoriesRoutes.get("/", (request, response) => {
+  return listCategoriesController.handle(request, response);
 });
-export { categories_routes };
+export { categoriesRoutes };
