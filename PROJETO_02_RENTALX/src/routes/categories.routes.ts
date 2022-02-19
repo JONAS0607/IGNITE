d0688ba9a4492468af50c00c28ab168f156868b8
Jonas Cabral <cabral.j@unoesc.edu.br>
2022-02-19 15:22:10 -0300

    Criando UseCase de categoria

--- PROJETO_02_RENTALX/src/routes/categories.routes.ts
+++ PROJETO_02_RENTALX/src/routes/categories.routes.ts
@@ -1,19 +1,13 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { PostgresCategoriesRepository } from "../modules/cars/repositories/PostgresCategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categories_routes = Router();
 const categories_repository = new CategoriesRepository();
 
 categories_routes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createCategoryService = new CreateCategoryService(
-    categories_repository
-  );
-  createCategoryService.execute({ name, description });
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 categories_routes.get("/", (request, response) => {
   const all = categories_repository.list();
