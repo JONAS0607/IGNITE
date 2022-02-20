7e0c61f75558c06e53f95aa0cc760aea9dca527e
Jonas Cabral <cabral.j@unoesc.edu.br>
2022-02-20 13:21:30 -0300

    erro csv-parse

--- PROJETO_02_RENTALX/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ PROJETO_02_RENTALX/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,15 +1,45 @@
-import { parse as csvParse } from "csv-parse";
+import { parse } from "csv-parse";
 import fs from "fs";
 
-class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
-    const parseFile = csvParse();
-    stream.pipe(parseFile);
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
-    parseFile.on("data", async (line) => {
-      console.log(line);
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = [];
+      const stream = fs.createReadStream(file.path);
+      const parseFile = parse();
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
     });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
   }
 }
 
