8b257d700e26c392694f3f6762a03600d4e10482
Jonas Cabral <cabral.j@unoesc.edu.br>
2022-02-28 12:24:16 -0300

    Criando repositório de usuário

---
+++ PROJETO_02_RENTALX/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    driver_license,
+    email,
+    name,
+    password,
+    username,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      driver_license,
+      email,
+      name,
+      password,
+      username,
+    });
+  }
+}
+
+export { CreateUserUseCase };
