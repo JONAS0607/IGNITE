dd009f41f52992f1d443242912d7550b413372f6
Jonas Cabral <cabral.j@unoesc.edu.br>
2022-04-08 18:28:06 -0300

    TDD na prática, criando testes para cars

---
+++ PROJETO_02_RENTALX/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,24 @@
+import { InMemoryCarsRepository } from "@modules/cars/repositories/in-memory/InMemoryCarsRepository";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: InMemoryCarsRepository;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new InMemoryCarsRepository();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+  it("Should be able to create a new car", async () => {
+    await createCarUseCase.execute({
+      name: "Name Car",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "Brand",
+      category_id: "category",
+    });
+  });
+});
