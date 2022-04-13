56a5c041c30b9d6fa045a8f6657c109c4b7daa28
Jonas Cabral <cabral.j@unoesc.edu.br>
2022-04-13 13:11:30 -0300

    Continuação criação de testes

--- PROJETO_02_RENTALX/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ PROJETO_02_RENTALX/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryCarsRepository } from "@modules/cars/repositories/in-memory/InMemoryCarsRepository";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
@@ -11,7 +12,7 @@ describe("Create Car", () => {
     createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
   });
   it("Should be able to create a new car", async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: "Name Car",
       description: "Description Car",
       daily_rate: 100,
@@ -20,5 +21,41 @@ describe("Create Car", () => {
       brand: "Brand",
       category_id: "category",
     });
+    expect(car).toHaveProperty("id");
+  });
+  it("Should not be able to create car with same license plate", async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Car 1",
+        description: "Description Car",
+        daily_rate: 100,
+        license_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "Brand",
+        category_id: "category",
+      });
+      await createCarUseCase.execute({
+        name: "Car 2",
+        description: "Description Car",
+        daily_rate: 100,
+        license_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "Brand",
+        category_id: "category",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+  it("Should not be able to create car with available false", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car Available",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "ABCD-1234",
+      fine_amount: 60,
+      brand: "Brand",
+      category_id: "category",
+    });
+    expect(car.available).toBe(true);
+    console.log(car);
   });
 });
